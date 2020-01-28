76a05379f091f3884e03eb7953b1d7e8d2d145e7
rllola <rllola80@gmail.com>
2020-01-28 19:05:15 +0100

    Need Sip 4.19.20 at least

--- scripts/download_libraries.sh
+++ scripts/download_libraries.sh
@@ -2,6 +2,6 @@ echo "Downloading libs"
 wget -q https://www.zlib.net/fossils/zlib-1.2.11.tar.gz
 wget -q http://download.qt.io/official_releases/qt/5.14/5.14.1/single/qt-everywhere-src-5.14.1.tar.xz
 wget -q https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tar.xz
-wget -q https://www.riverbankcomputing.com/static/Downloads/sip/4.19.18/sip-4.19.18.tar.gz
+wget -q https://www.riverbankcomputing.com/static/Downloads/sip/4.19.20/sip-4.19.20.tar.gz
 wget -q https://files.pythonhosted.org/packages/3a/fb/eb51731f2dc7c22d8e1a63ba88fb702727b324c6352183a32f27f73b8116/PyQt5-5.14.1.tar.gz
 wget -q https://ftp.openssl.org/source/old/1.1.0/openssl-1.1.0j.tar.gz
